dc936cd4c6580ca395b3845e854b9de063251396
etrickel <erik.trickel@asu.edu>
2023-12-16 13:35:43 -0700

    WiP

--- testtester.sh
+++ testtester.sh
@@ -38,7 +38,7 @@ trap on_exit EXIT
 
 echo "Testing #${testcase} testing Working = ${testPositive}" >> DEBUG 
 
-if -q grep "REPLACE_ME" test?.sh; then 
+if grep -q "REPLACE_ME" test?.sh; then 
     echo "The test1.sh and test2.sh must have the REPLACE_ME removed from everywhere even comments if it somehow got into the comments" >> DEBUG 
     echo "np" > RESULT
     exit 1
