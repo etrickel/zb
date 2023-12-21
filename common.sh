087aaa1a87d334ae9c897f99671a74cc4751cc3a
etrickel <erik.trickel@asu.edu>
2023-12-21 11:07:59 -0700

    WiP

--- common.sh
+++ common.sh
@@ -127,7 +127,7 @@ function testInputOutputv2(){
                 if [[ ! -f /tmp/COMP_EXPECTED ]] ; then
                     echo $EXPECTED > /tmp/COMP_EXPECTED
                 fi     
-                icdiff /tmp/COMP_EXPECTED /tmp/OUTPUT
+                icdiff /tmp/COMP_EXPECTED /tmp/OUTPUT >> DEBUG
                 
             fi 
             echo "np" > RESULT 
