e7b6a368f4d491d65c6760f3f7f221a0dab83062
etrickel <erik.trickel@asu.edu>
2023-12-26 12:39:19 -0700

    WiP

--- common.sh
+++ common.sh
@@ -266,7 +266,7 @@ function verifyInOrder()
         if [[ -z "$line" ]]; then
             printf "\033[38;5;1mFAILED to find '${str}' in the proper order \033[0m\n" >> DEBUG 
             printf "Expected order of values are " >> DEBUG 
-            IFS=","; printf "${order[*]}" | grep --color=auto "${str}" >> DEBUG 
+            IFS=","; printf "${order[*]}" | grep "${str}" >> DEBUG 
             printf "\n" >> DEBUG 
             exit 1
         else
