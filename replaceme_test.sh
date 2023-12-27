f1870013d77fd0697d3da35031ad7dc9237f1b3a
etrickel <erik.trickel@asu.edu>
2023-12-27 14:53:45 -0700

    WiP

--- replaceme_test.sh
+++ replaceme_test.sh
@@ -1,8 +1,18 @@
-if grep -q "REPLACE_ME" test?.sh; then 
-    echo "The test1.sh and test2.sh must have the REPLACE_ME removed from everywhere even comments if it somehow got into the comments"
-    echo "np" > RESULT
-    exit 1
-else
-    echo "Test Cases Pass REPLACE_ME test."    
-    echo "p" > RESULT
+
+if [[ -f test1.sh ]]; then 
+    if grep -q "REPLACE_ME" test?.sh; then 
+        echo "The tests must have the REPLACE_ME removed from everywhere even comments if it somehow got into the comments"
+        echo "np" > RESULT
+        exit 1
+    fi 
 fi 
+if [[ -f test1.c ]]; then 
+    if grep -q "REPLACE_ME" test.c; then 
+        echo "The file test.c must have the REPLACE_ME removed from everywhere even comments if it somehow got into the comments"
+        echo "np" > RESULT
+        exit 1
+    fi 
+fi 
+
+echo "Test Cases Pass REPLACE_ME test."    
+echo "p" > RESULT
