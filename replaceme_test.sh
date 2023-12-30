a78970953cdef6cb926c967da5289288a27f4a54
etrickel <erik.trickel@asu.edu>
2023-12-30 09:55:41 -0700

    WiP

--- replaceme_test.sh
+++ replaceme_test.sh
@@ -1,4 +1,23 @@
 
+function verifyAllTestFiles() {
+    local fileCount=$1
+    if (( $(ls test?.sh 2> /dev/null |wc -l) < $fileCount )); then
+        printf "Failing test because your instance is missing test scripts from the template, please make sure that all test files are included in your workspace\n" >> DEBUG
+        echo "np"
+        exit 33
+    fi 
+
+    for (( x=1; x <= $fileCount; x++ )); do 
+        if grep -q "REPLACE_ME" test${x}.sh; then 
+            echo "The tests must have the REPLACE_ME removed from everywhere even comments if it somehow got into the comments"
+            echo "Found REPLACE_ME in test${x}.sh please create your test case "
+            echo "np" > RESULT
+            exit 1
+        fi         
+    done 
+
+}
+
 if [[ -f test1.sh ]]; then 
     if grep -q "REPLACE_ME" test?.sh; then 
         echo "The tests must have the REPLACE_ME removed from everywhere even comments if it somehow got into the comments"
